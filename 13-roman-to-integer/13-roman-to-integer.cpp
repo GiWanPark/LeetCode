e1a97f2c6e564e38fd786f00725f83fa8358e518
Kiwan Park <aaamy91@naver.com>
2022-06-13 23:02:37 +0100

    Time: 15 ms (57.11%), Space: 8.1 MB (29.21%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int roman = 0;
+        const std::map<char, int> RomanMap = { 
+            {'I', 1}
+        ,   {'V', 5}
+        ,   {'X', 10} 
+        ,   {'L', 50} 
+        ,   {'C', 100} 
+        ,   {'D', 500} 
+        ,   {'M', 1000} };
+
+        for (int i = 0; i < s.size(); ++i)
+        {
+            if (i + 1 < s.size())
+            {
+                if (RomanMap.find(s[i])->second < RomanMap.find(s[i + 1])->second)
+                {
+                    roman += RomanMap.find(s[i + 1])->second - RomanMap.find(s[i])->second;
+                    ++i;
+                    continue;
+                }
+            }
+            
+            roman += RomanMap.find(s[i])->second;
+        }
+
+        return roman;
+    }
+};
