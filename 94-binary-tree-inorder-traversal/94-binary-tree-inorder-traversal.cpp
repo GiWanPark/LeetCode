665cbc70719458c0816c44352400f4127d215952
Kiwan Park <aaamy91@naver.com>
2022-07-03 22:32:00 +0100

    Time: 5 ms (24.14%), Space: 8.5 MB (40.75%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -12,51 +12,15 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-		vector<int> result;
-		
-		stack<TreeNode*> stk;
-		while (root != nullptr)
-		{
-			if (root->left != nullptr)
-			{
-				stk.push(root);
-				root = root->left;
-				continue;
-			}
-			else
-			{
-				if(root->val != -101)
-					result.push_back(root->val);
-				root->val = -101;
-			}
-
-			if (root->right != nullptr)
-			{
-				stk.push(root);
-				root = root->right;
-				continue;
-			}
-			else if (stk.empty())
-			{
-				break;
-			}
-			else
-			{
-				if (root == stk.top()->left)
-				{
-					delete stk.top()->left;
-					stk.top()->left = nullptr;
-				}
-				else if(root == stk.top()->right)
-				{
-					delete stk.top()->right;
-					stk.top()->right = nullptr;
-				}
-				root = stk.top();
-				stk.pop();
-			}
-		}
-		
-		return result;
-	}
+        vector<int> nodes;
+        std::stack<TreeNode*> toVisit;
+        while(1) {
+            while(root) { toVisit.push(root); root=root->left; }
+            if(toVisit.empty()) break;
+            root=toVisit.top(); toVisit.pop();
+            nodes.push_back(root->val);
+            root=root->right;
+        }
+        return nodes;
+    }
 };
